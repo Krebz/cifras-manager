6c6c2d4d8ce7403e02c5f1af5dca8117afb31730
Kleber <kleber_kma@hotmail.com>
2026-06-21 00:16:33 -0300

    fix: add .js extension to ESM imports in api routes

--- api/songs/[id].ts
+++ api/songs/[id].ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { ObjectId } from "mongodb";
-import { getDb } from "../lib/mongodb";
+import { getDb } from "../lib/mongodb.js";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { id } = req.query;
